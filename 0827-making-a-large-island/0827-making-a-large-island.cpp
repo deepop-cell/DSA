164ceb8e76dadb262e40433e38a093f35293c8bb
deepop-cell <bandana.0081@gmail.com>
2026-06-25 14:42:26 +0530

    Time: 251 ms (55.08%), Space: 152.1 MB (47.23%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,20 +1,20 @@
+
 class Solution {
 public:
-    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
     int dfs(vector<vector<int>>& grid, int i, int j, vector<vector<bool>>& vis, int id, int n) {
         if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] == 0 || vis[i][j]) {
             return 0;
         }
-        int size = 1;
+        
         vis[i][j] = true;
         grid[i][j] = id;
-        for (auto &it : directions) {
-            int i_ = i + it[0];
-            int j_ = j + it[1];
-            size += dfs(grid, i_, j_, vis, id, n);
-        }
-        return size;
+
+        int up    = dfs(grid, i - 1, j, vis, id, n);
+        int down  = dfs(grid, i + 1, j, vis, id, n);
+        int left  = dfs(grid, i, j - 1, vis, id, n);
+        int right = dfs(grid, i, j + 1, vis, id, n);
+
+        return 1 + up + down + left + right;
     }
 
     int largestIsland(vector<vector<int>>& grid) {
@@ -24,6 +24,7 @@ public:
         unordered_map<int, int> mp;
         int maxar = 0;
         bool hasZero = false; 
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1 && !vis[i][j]) {
@@ -34,16 +35,17 @@ public:
                 }
             }
         }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
                     hasZero = true; 
                     unordered_set<int> st;
                     
-                    if (i + 1 < n && grid[i + 1][j] != 0) st.insert(grid[i + 1][j]);
-                    if (i > 0 && grid[i - 1][j] != 0) st.insert(grid[i - 1][j]);
-                    if (j + 1 < n && grid[i][j + 1] != 0) st.insert(grid[i][j + 1]);
-                    if (j > 0 && grid[i][j - 1] != 0) st.insert(grid[i][j - 1]);
+                    if (i - 1 >= 0 && grid[i - 1][j] != 0) st.insert(grid[i - 1][j]); 
+                    if (i + 1 < n  && grid[i + 1][j] != 0) st.insert(grid[i + 1][j]); 
+                    if (j - 1 >= 0 && grid[i][j - 1] != 0) st.insert(grid[i][j - 1]); 
+                    if (j + 1 < n  && grid[i][j + 1] != 0) st.insert(grid[i][j + 1]); 
 
                     int overall_size = 1; 
                     for (auto &it : st) {
@@ -53,7 +55,7 @@ public:
                 }
             }
         }
-
+        
         return (!hasZero) ? n * n : maxar;
     }
 };
