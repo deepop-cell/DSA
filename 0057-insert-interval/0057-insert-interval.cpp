35bf639ddd01cd0b2e0acd15e0301b1c323ff625
deepop-cell <bandana.0081@gmail.com>
2026-06-25 20:57:04 +0530

    Time: 0 ms (100%), Space: 21.8 MB (47%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n=intervals.size();
-        vector<vector<int>>result;
-        int i=0;
-        while(i<n && intervals[i][1]<newInterval[0]){
-            result.push_back(intervals[i]);
-            i++;
-        }
-        //pushed all the intervals before it
-    //no wmerge
+    vector<vector<int>>result;
+    int i=0;
+    while(i<n && intervals[i][1]<newInterval[0]){
+        result.push_back(intervals[i]);
+        i++;
+    }
     while(i<n && intervals[i][0]<=newInterval[1]){
         newInterval[0]=min(newInterval[0],intervals[i][0]);
         newInterval[1]=max(newInterval[1],intervals[i][1]);
@@ -20,9 +18,7 @@ public:
         result.push_back(intervals[i]);
         i++;
     }
-    return result;
 
-
-        
+        return result;
     }
 };
