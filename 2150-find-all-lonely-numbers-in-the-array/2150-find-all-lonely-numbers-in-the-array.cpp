984ee32373a6053ff93f387edd0ecc9fc8fda16c
deepop-cell <bandana.0081@gmail.com>
2026-06-26 18:01:11 +0530

    Time: 165 ms (61.14%), Space: 171.6 MB (47.94%) - LeetHub

---
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findLonely(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int x:nums){
+            mp[x]++;
+        }
+        vector<int>result;
+        for(auto it:mp){
+            if(it.second==1 && mp.find(it.first+1)==mp.end() && mp.find(it.first-1)==mp.end()){
+                result.push_back(it.first);
+
+            }
+        }
+        return result;
+    }
+};
