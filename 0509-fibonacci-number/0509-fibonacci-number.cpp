17a349529e7e2524219b5499f01bb3fb44ddb18d
Deep Pandey <bandana.0081@gmail.com>
2026-06-30 13:44:11 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,16 +4,16 @@ public:
         if(n <= 1){
             return n;
         }
-        
-        vector<int> dp(n + 1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        
-        for(int i = 2; i < n + 1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        int a=0;
+        int b=1;
+        int c;
+        for(int i = 1; i < n ; i++){
+            c=a+b;
+            a=b;
+            b=c;
         }
         
-        return dp[n]; 
+        return c; 
     }
 };
 
