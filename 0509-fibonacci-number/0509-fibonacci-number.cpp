da8a3e490893fc594572ed8c19f4bc5b0ac9f4f0
Deep Pandey <bandana.0081@gmail.com>
2026-06-30 12:33:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,24 @@
-class Solution {
-public:
+class Solution { 
+public: 
+    int solve(int n, vector<int>& dp) { 
+        if (n <= 1) {
+            return n;
+        }
+        
+        // Solved hai already to use karlo na yaar!
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+    
     int fib(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        else if (n == 1) {
-            return 1;
-        }
-        else {
-            return fib(n - 1) + fib(n - 2);
-        }
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
+
