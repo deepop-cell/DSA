ff9fc4b4e2efe75a9603924e179904681c0bcdd4
Deep Pandey <bandana.0081@gmail.com>
2026-07-04 19:34:27 +0530

    Time: 280 ms (61.46%), Space: 103 MB (59.03%) - LeetHub

--- 2926-maximum-balanced-subsequence-sum/2926-maximum-balanced-subsequence-sum.cpp
+++ 2926-maximum-balanced-subsequence-sum/2926-maximum-balanced-subsequence-sum.cpp
@@ -5,25 +5,24 @@ public:
         map<int,long long>mp;
         long long result=INT_MIN;
         for(int i=0;i<n;i++){
-         
-         auto it=mp.upper_bound(nums[i]-i); //jis bhi i par ho usse just bada wala nums[i]-i pe point krega ye 
-         long long sum=nums[i];//jis i par khade ho.
-         if(it!=mp.begin()){
-            it--;//now <= wale pe point krega  
-            sum+=it->second;
-         }
-         //ab iski entry krdo.
-         mp[nums[i]-i]=max(sum,mp[nums[i]-i]);
+            long long sum=nums[i];
+            auto it=mp.upper_bound(nums[i]-i);
+            if(it!=mp.begin()){
+                //>=nums[i]-i. milgaya hai.
+                it--;
+                sum+=it->second;
+                //entry bhi kardo
 
-         //ab wo delete krna hai jiska nums[i]-i se bada ho aur sum chota ho.
-         it=mp.upper_bound(nums[i]-i);
-         while(it!=mp.end() && it->second <=sum){
-            mp.erase(it++);
-         }
-
-         result=max(result,sum);
+            }
+            mp[nums[i]-i]=max(mp[nums[i]-i],sum);//jo bhi bada ho usko lnea hai.
+            //entry ke baad hume wo nums[i]-i jiska value bada hia par sum chota hai wo useles hai use dlete krdo
+            it=mp.upper_bound(nums[i]-i);
+            while(it!=mp.end() && it->second<=sum){
+                mp.erase(it++);
+            }
+            //ab useless delete ho gye hai 
+            result=max(result,sum);
         }
-        return result;
+     return result;
         }
- 
 };
