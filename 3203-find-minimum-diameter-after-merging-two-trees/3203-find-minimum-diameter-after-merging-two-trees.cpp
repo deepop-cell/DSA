1dc7b71f0d76ce9cd4b1b981585523211e649c27
Deep Pandey <bandana.0081@gmail.com>
2026-07-04 22:11:40 +0530

    Time: 533 ms (33.33%), Space: 367.4 MB (30.58%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,53 +1,54 @@
 class Solution {
 public:
-int diameter(vector<vector<int>>&edges,int node,int&level){
+int diameter(vector<vector<int>>&edges,int node,int &level){
     int E=edges.size();
-    int n=E+1;
+    int n=E+1;//no of nodes =edges.size()+1
     vector<vector<int>>adj(n);
-    for(auto &e:edges){
-        int u=e[0];
-        int v=e[1];
+    vector<bool>vis(n+1,false);
+    for(int i=0;i<E;i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
         adj[u].push_back(v);
-        adj[v].push_back(u);
-    }//adjacency list.
-    queue<int>q;
-    vector<bool>vis(n,false);
-    q.push(node);
+        adj[v].push_back(u);//undriected graph.
+    }
     int farthestnode=node;
+    queue<int>q;
+    q.push(node);
     vis[node]=true;
     while(!q.empty()){
         int sz=q.size();
         for(int i=0;i<sz;i++){
-            int currnode=q.front();
-            farthestnode=currnode;
+            farthestnode=q.front();
             q.pop();
-            for(auto &v:adj[currnode]){
+            for(auto &v:adj[farthestnode]){
                 if(!vis[v]){
                     q.push(v);
-                    vis[v]=true;//mark true while pushing
+                    vis[v]=true;
                 }
             }
+
         }
         if(!q.empty()){
-                level++;
+            level++;
         }
     }
     return farthestnode;
 
-
 }
+
+
     int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
         int x=0;
-        int one_end_edges1=diameter(edges1,0,x);
+        int one_edge_d1=diameter(edges1,0,x);
         int d1=0;
-        diameter(edges1,one_end_edges1,d1);//this will make d1.
+        diameter(edges1,one_edge_d1,d1);
         int y=0;
-        int one_end_edges2=diameter(edges2,0,y);
+        int one_edge_d2=diameter(edges2,0,y);
         int d2=0;
-        diameter(edges2,one_end_edges2,d2);
-        //return (d1+1)/2+(d2+1)/2+1;(this missed some test cases), because aoisa bhi ho skta ki diamater (yani longest path trees merge krne ke baad d1, ya d2 mai se  koi ek ho)
+        diameter(edges2,one_edge_d2,d2);
         int dmax=max(d1,d2);
-        int dmerge=(d1+1)/2+(d2+1)/2+1;
-        return max(dmax,dmerge);
+        return max(dmax,(d1+1)/2 +(d2+1)/2+1);
+
+      
     }
 };
