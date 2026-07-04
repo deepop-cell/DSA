4f25dabc2e54e8355c33ca18fcbedc7211e0e113
Deep Pandey <bandana.0081@gmail.com>
2026-07-04 12:54:42 +0530

    Time: 67 ms (57.01%), Space: 14.2 MB (70.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-int dp[2501][2501];
-int solve(int idx,int prev,vector<int>&nums){
-    if(idx>=nums.size()){
-        return 0;//out of bounds hai.
-    }
-    if(dp[idx][prev+1]!=-1){//intially prev -1 hai to array mai -1 index thodi hoga to isliye prev+1.
-        return dp[idx][prev+1];
-    }
-    int take=0;
-    if(prev==-1 || nums[prev]<nums[idx]){
-        //take krna hai .(p wala index ka chota hona chaiye for increasing subsequence).
-         take=1+solve(idx+1,idx,nums);//ab add krlia hai to prev ke badle idx jayga
-    }
-    //skip krna hai(no special condition required)
-    int skip=solve(idx+1,prev,nums);
-    return dp[idx][prev+1]=max(take,skip);//wo -1 hai initially to isliye prev+1
-}
-
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         int n=nums.size();
-        return solve(0,-1,nums);
+        vector<int>dp(n,1);
+        //dp[i]=lis ending at ith index
+        int maxlis=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                //for making increasing subsequence.
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    maxlis=max(maxlis,dp[i]);///keep updating the max lis lenght.
+                }
+            }
+        }
+        return maxlis;
+     
         
     }
 };
