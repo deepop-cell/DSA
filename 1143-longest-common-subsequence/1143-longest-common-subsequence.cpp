2fd8c0d5a881fce48cffb538246b59d85951cf9e
Deep Pandey <bandana.0081@gmail.com>
2026-07-07 12:13:59 +0530

    Time: 27 ms (56.83%), Space: 27.3 MB (77.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-int dp[1001][1001];
-int solve(string &s1,string &s2,int i,int j){
-    if(i>=s1.length() || j>=s2.length()){
-        return 0;
-    }
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    //ab manlo dono ke index par same element hai.
-    if(s1[i]==s2[j]){
-        return dp[i][j]=1+solve(s1,s2,i+1,j+1);//dono mai aage badh jao..
-    }
-    else{
-        //s1!=s2 at i,j then ek ko aage badhao dusre ko nahi and max nikalo
-        int include_i=solve(s1,s2,i,j+1);
-        int include_j=solve(s1,s2,i+1,j);
-        return dp[i][j]=max(include_i,include_j);
-    }
-}
     int longestCommonSubsequence(string text1, string text2) {
-        memset(dp,-1,sizeof(dp));
-        return solve(text1,text2,0,0);
+        //bottom up 
+        int m=text1.length();
+        int n=text2.length();
+        vector<vector<int>>dp(m+1,vector<int>(n+1));
+        //state definition dp[i][j]=lcs of s1 of lentgh i and s2 of lentgh j .(note length hai index nahi ....)
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                //agar i ya j mai se ek b 0 hai to 0..
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+                
+                //manlo equal ho gye to .
+                else if(text1[i-1]==text2[j-1]){
+                    //dono mai aage badh jao.( bottom up mai peeche jaate hai)
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    //unequal hai to ek ko move kro dusre ko nhi and max  nikalo
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];// this returns lcs of s1 of lentgh m and s2 of lentgh n
+        
     }
 };
