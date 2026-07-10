03ccf64c9957b41be88d1795575f754742fb3fd6
Deep Pandey <bandana.0081@gmail.com>
2026-07-10 10:29:08 +0530

    Time: 116 ms (5.02%), Space: 75.9 MB (44.41%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        int n=s.length();
-        string rev=s;
-        reverse(rev.begin(),rev.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1));
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-                else if(s[i-1]==rev[j-1]){
-                    //equal hai to diagonally jao.
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                //unequal hai to max side jao ..
+      //idea ye hai ki . lps is lcs of string and its reverse.
+      string x=s;
+      reverse(x.begin(),x.end());
+      //state def : dp[i][j]=LCS len of s1 of len i and s2 of len j/...
+      int n=s.length();
+      vector<vector<int>>dp(n+1,vector<int>(n+1));
+      for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            //agar ek b string empty hua to lcs 0 ho jayga.
+            if(i==0 || j==0 ){
+                dp[i][j]=0;
+            }
+            else if(s[i-1]==x[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                //unequl hai..
                 dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
             }
         }
-        return dp[n][n];
+      }
+      return dp[n][n];
     }
 };
