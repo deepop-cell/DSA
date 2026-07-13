fe7258c103fbb8d552b024505dafd12398140505
Deep Pandey <bandana.0081@gmail.com>
2026-07-13 22:49:59 +0530

    Time: 3 ms (96.44%), Space: 32.9 MB (95.17%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,33 +1,55 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
         if(head==NULL){
-            return NULL;
+            return nullptr;
         }
         else if(head->next==NULL){
-          TreeNode* root=new TreeNode(head->val);
-          return root;
+            TreeNode* root=new TreeNode(head->val);
+            return root;
         }
+        //middle node find kro..
         ListNode* slow=head;
+        ListNode*  fast=head;
         ListNode* slow_prev=NULL;
-        ListNode* fast=head;
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
             if(slow_prev!=NULL){
                 slow_prev=slow_prev->next;
             }
-            else if(slow_prev==NULL){
+            else{
                 slow_prev=head;
             }
         }
+        // ab jo slow hai wo middle hai...
         TreeNode* root=new TreeNode(slow->val);
-        slow_prev->next=NULL;//breaking links
         ListNode* temp=slow->next;
-        slow->next=NULL;//breakking links
+        slow_prev->next=NULL;
         root->left=sortedListToBST(head);
         root->right=sortedListToBST(temp);
         return root;
+        
     }
 };
