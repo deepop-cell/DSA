0a2af90c4658a361c55427f651e6d3b20e35cc1e
Deep Pandey <bandana.0081@gmail.com>
2026-07-15 10:41:27 +0530

    Time: 3 ms (11.89%), Space: 9.3 MB (26.04%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int i=0;
+        while(1){
+            if(s[i]==t[i]){
+                i++;
+            }
+            else{
+                return t[i];
+            }
+        }
+        
+    }
+};
