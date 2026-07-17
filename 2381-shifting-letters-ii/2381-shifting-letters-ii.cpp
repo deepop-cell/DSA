1663578037c17d739a177e04ddeeb6942e20f135
Deep Pandey <bandana.0081@gmail.com>
2026-07-17 19:06:15 +0530

    Time: 1 ms (84.14%), Space: 97.9 MB (52.18%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        //range based updating ki baat ho rahi hai.. therefore difference array technique use krenge.
         int n=s.length();
         vector<int>res(n,0);
-        for(auto &x:shifts){
+        for(auto &x : shifts){
             int start=x[0];
             int end=x[1];
-            int dir=x[2]; 
-                res[start]+=(dir==1)?1:-1;
+            int dir=x[2];
+            if(dir==1){
+                //matlab ahead...
+                res[start]+=1;
                 if(end+1<n){
-                    res[end+1]-=(dir==1)?1:(-1*1);
+                    res[end+1]-=1;
                 }
+            }
+            else{
+                res[start]-=1;
+                if(end+1<n){
+                    res[end+1]+=1;
+                }
+            }
         }
-     int csum=0;
-     for(int i=0;i<n;i++){
-        csum+=res[i];
-        res[i]=csum;
-     }   
-     //ab res[i]=net shift of ith element of the string,..
-     for(int i=0;i<n;i++){
-        int netshift=res[i]%26;
-        s[i]='a'+(s[i]-'a'+netshift+26)%26;
-     }
-     return s;
+        //now do csum thing on res..
+        int csum=0;
+        for(int i=0;i<n;i++){
+            csum+=res[i];
+            res[i]=csum;
+        }
+        //ab res[i] is denoting the net shift in ith index of the string..
+        for(int i=0;i<n;i++){
+            int shift=res[i];
+            shift=shift%26;
+            if(shift<0){
+                shift+=26;
+            }
+            s[i]='a'+(s[i]-'a'+shift)%26;
+        }
+        return s;
     }
 };
