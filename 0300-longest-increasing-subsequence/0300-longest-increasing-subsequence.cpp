868ab62e0e691974c80a1a087cafd77c6775e7ae
Deep Pandey <bandana.0081@gmail.com>
2026-07-18 16:38:33 +0530

    Time: 450 ms (24.74%), Space: 41.6 MB (34.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
+int dp[2501][2501];
+int solve(int idx,int previdx,vector<int>&nums){
+    if(idx==nums.size()){
+        return 0;
+    }
+    if(dp[idx][previdx+1]!=-1){
+        return dp[idx][previdx+1];
+    }
+    //for take 
+    int take=0;
+    if(previdx==-1 || nums[previdx]<nums[idx]){
+        take=1+solve(idx+1,idx,nums);
+    }
+    //skip 
+    int skip=solve(idx+1,previdx,nums);
+    return dp[idx][previdx+1]=max(take,skip);
+}
     int lengthOfLIS(vector<int>& nums) {
-        int  n=nums.size();
-        int mx=1;
-        vector<int>dp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[j]+1,dp[i]);
-                }
-                mx=max(mx,dp[i]);
-            }
-        }
-        return mx;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,-1,nums);
+
         
     }
 };
