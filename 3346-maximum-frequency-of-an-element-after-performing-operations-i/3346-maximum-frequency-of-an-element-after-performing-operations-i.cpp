74e0dcbdef42aa899769c29a09f2f6e5a5b4d7f8
Deep Pandey <bandana.0081@gmail.com>
2026-07-18 16:27:33 +0530

    Time: 275 ms (41.47%), Space: 192.8 MB (30.78%) - LeetHub

---
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int maxVal = *max_element(begin(nums), end(nums)) + k;
+        vector<int> diff(maxVal+2, 0);
+        unordered_map<int, int> freq;
+        for(int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+            int l = max(nums[i]-k, 0);
+            int r = min(nums[i]+k, maxVal);
+            diff[l]++;
+            diff[r+1]--;
+        }
+        int result = 1;
+        for(int target = 0; target <= maxVal; target++) {
+            diff[target] += (target > 0 ? diff[target-1] : 0);//csum kar rhe hai. int csum=0 and thaty method is alspo rioght
+            int targetFreq     = freq[target];
+            int needConversion = diff[target] - targetFreq;
+            int maxPossibleFreq = min(needConversion, numOperations);
+            result = max(result, targetFreq + maxPossibleFreq);
+        }
+        return result;
+    }
+};
