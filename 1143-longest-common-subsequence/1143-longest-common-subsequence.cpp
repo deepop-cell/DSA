4f34ad807c9eaea674411147a9dc633a900cfd25
Deep Pandey <bandana.0081@gmail.com>
2026-07-19 19:51:30 +0530

    Time: 28 ms (43.42%), Space: 27.4 MB (54.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        //bottom up 
-        int m=text1.length();
-        int n=text2.length();
-        vector<vector<int>>dp(m+1,vector<int>(n+1));
-        //state definition dp[i][j]=lcs of s1 of lentgh i and s2 of lentgh j .(note length hai index nahi ....)
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                //agar i ya j mai se ek b 0 hai to 0..
+        //state definiton dp[i][j]= longest commmon susbeuqnece len of s1 of len i and s2 of len j
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
+        //now fill this dp 2d array and return dp[n][m];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
                 if(i==0 || j==0){
                     dp[i][j]=0;
                 }
-                
-                //manlo equal ho gye to .
-                else if(text1[i-1]==text2[j-1]){
-                    //dono mai aage badh jao.( bottom up mai peeche jaate hai)
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
                 else{
-                    //unequal hai to ek ko move kro dusre ko nhi and max  nikalo
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    if(text1[i-1]==text2[j-1]){
+                        //equal hai to increment len by 1 because common hai and move in both 
+                        dp[i][j]=1+dp[i-1][j-1];
+                    }
+                    else{
+                        //unequal hai to move towards the side jo maximum length de sake asani  se,,
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    }
                 }
             }
         }
-        return dp[m][n];// this returns lcs of s1 of lentgh m and s2 of lentgh n
-        
+        return dp[n][m];
     }
 };
