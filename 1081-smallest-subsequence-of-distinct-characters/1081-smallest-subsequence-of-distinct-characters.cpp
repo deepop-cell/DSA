49e6cbb6e23bed27df0a34107a84b9fd397678c0
Deep Pandey <bandana.0081@gmail.com>
2026-07-19 19:46:33 +0530

    Time: 0 ms (100%), Space: 8.7 MB (57.91%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
+        int n=s.length();
         vector<bool>taken(26,false);
         vector<int>lastvis(26,-1);
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<n;i++){
             lastvis[s[i]-'a']=i;
         }
+        //now make a string where u will make a subsequence..
         string ans="";
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<n;i++){
             char ch=s[i];
-            if(taken[ch-'a']){
-                //taken hai to skip kaardo.. element already present in the given string..
-                continue;
+            if(taken[s[i]-'a']){
+                continue;//skip if already taken this element in the subsequence,..
             }
-            //this hting is to maintain lexicographical order for the subseuquence.//
+            //now for lexicographcal order of subseuqeucne do this,
             while(!ans.empty() && ans.back()>ch && lastvis[ans.back()-'a']>i){
                 taken[ans.back()-'a']=false;
                 ans.pop_back();
             }
+            //ab u can take this current element safely by maintaining lexicorgpahic order and no repeating characters..
             ans.push_back(ch);
+            //ab dal rhe ho subsequence mai to mark vis bhi to kardo bhai sath hee sath
             taken[ch-'a']=true;
-
         }
         return ans;
     }
