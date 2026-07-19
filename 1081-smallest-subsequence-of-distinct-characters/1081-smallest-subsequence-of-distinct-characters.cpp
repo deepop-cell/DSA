a36aa7b9550e899c280c5d153044be895e230901
Deep Pandey <bandana.0081@gmail.com>
2026-07-19 15:34:41 +0530

    Time: 0 ms (100%), Space: 8.8 MB (34%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -3,7 +3,6 @@ public:
     string smallestSubsequence(string s) {
         vector<bool>taken(26,false);
         vector<int>lastvis(26,-1);
-        //filling lastvis
         for(int i=0;i<s.length();i++){
             lastvis[s[i]-'a']=i;
         }
@@ -11,16 +10,18 @@ public:
         for(int i=0;i<s.length();i++){
             char ch=s[i];
             if(taken[ch-'a']){
+                //taken hai to skip kaardo.. element already present in the given string..
                 continue;
             }
+            //this hting is to maintain lexicographical order for the subseuquence.//
             while(!ans.empty() && ans.back()>ch && lastvis[ans.back()-'a']>i){
                 taken[ans.back()-'a']=false;
                 ans.pop_back();
             }
             ans.push_back(ch);
             taken[ch-'a']=true;
+
         }
         return ans;
-        
     }
 };
