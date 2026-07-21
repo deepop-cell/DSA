8eb9a79060ee322e4dc060e6d31c67d7045bcea8
Deep Pandey <bandana.0081@gmail.com>
2026-07-21 11:39:59 +0530

    Time: 6 ms (71.21%), Space: 93.4 MB (54.98%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums, int limit) {
-        int n=nums.size();
         vector<int>diff(2*limit+2,0);
+        int n=nums.size();
         for(int i=0;i<n/2;i++){
             int a=nums[i];
             int b=nums[n-1-i];
+            int minval=1+min(a,b);
+            int maxval=limit+max(a,b);
+
+            //start to end tk +2 krna hai..(because else ke lie wo hi hai,.)
             diff[2]+=2;
             diff[2*limit+1]-=2;
-            //ab is pair se maxval aur min val kya bana skte wo find kro..
-            int minval=1+min(a,b);//max of a,b ko convert krdo 1 mai and min bachgya
-            int maxval=limit+max(a,b);//min ko limit mai convert krdo aur max bachgya.
-            //now we have range[minva,maxval]. isme sare ke lie moves =1 lgega.. except when a+b = targetsum..
-            //for moves=1;
-            diff[minval]+=-1;
-            diff[maxval+1]-=(-1);
 
-            //for moves=0;
-            diff[a+b]-=1;
-            diff[a+b+1]-=(-1);//inko aur ek se ghata do taki intial 2 se ye 0 ho jaye because movesis 0 for them.
+            //ab jao for moves=1;
+            diff[minval]+=(-1);
+            diff[maxval+1]-=(-1);//inke lie 2 tha but ispar 1 hona chaiye so further reduced by 1.
+
+            //ab for moves=0;
+            diff[a+b]+=(-1);
+            diff[a+b+1]-=(-1);
         }
-            int result=INT_MAX;
+        int res=INT_MAX;
             int csum=0;
-            for(int target=2;target<=2*limit;target++){
-                csum+=diff[target];
-                diff[target]=csum;
-                result=min(result,diff[target]);
-            }
-        return result;
+        for(int target=2;target<=2*limit;target++){
+            csum+=diff[target];
+            diff[target]=csum;
+            res=min(res,diff[target]);
+        }
+        return res;
     }
 };
