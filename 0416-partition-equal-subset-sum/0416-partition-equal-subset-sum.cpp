10b188295503f1abeeb22068b4713e96d5fc42fd
Deep Pandey <bandana.0081@gmail.com>
2026-07-23 12:57:04 +0530

    Time: 174 ms (19.85%), Space: 17.5 MB (59.63%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-//just find if there exists a subset of this whose sum =total arr wt/2.
-int dp[201][10001];
-int solve(int i,vector<int>&nums,int sum){
-    if(sum==0){
-        return true;
-    }
-    if(sum!=0 && i==nums.size()){
-        return false;
-    }
-    if(dp[i][sum]!=-1){
-        return dp[i][sum];
-    }
-    bool take=false;
-    bool skip=false;
-    if(nums[i]<=sum){
-        //then u can take.
-        take=solve(i+1,nums,sum-nums[i]);
-    }
-    skip=solve(i+1,nums,sum);
-    return dp[i][sum]=  ( skip || take);//auto casts bool into 0/1.
-}
     bool canPartition(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        int totalwt=0;
-        for(int x:nums){
-            totalwt+=x;
+        ///bottom up.
+        //dp[i][j]= is it possible for first i elements to have a subset sum equals to j..
+        int n=nums.size();
+        int targetsum=0;
+        for(auto &x:nums){
+            targetsum+=x;
         }
-        if(totalwt%2!=0){
-            //total wt odd hai to equal sum mai split hee nahi kr skte ho,
+        if(targetsum%2!=0){
             return false;
         }
-        int targetsum=totalwt/2;
-        return solve(0,nums,targetsum);
+        targetsum/=2;
+        vector<vector<bool>>dp(n+1,vector<bool>(targetsum+1));
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=true;//sum 0 means found na!!!
+        }
+        for(int j=0;j<targetsum+1;j++){
+            dp[0][j]=false;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<targetsum+1;j++){
+                bool take=false;
+                bool skip=false;
+                if(nums[i-1]<=j){
+                    take=dp[i-1][j-nums[i-1]];
+                }
+                skip=dp[i-1][j];
+                dp[i][j]=(skip|| take);
+            }
+        }
+        return dp[n][targetsum];
     }
 };
