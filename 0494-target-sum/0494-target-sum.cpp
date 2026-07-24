f7c53614b0fa68b6d339dbe770c92a4d88c146e4
Deep Pandey <bandana.0081@gmail.com>
2026-07-24 18:06:24 +0530

    Time: 0 ms (100%), Space: 11.8 MB (75.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
 int dp[21][1001];
-int solve(int i,vector<int>&nums,int sum){
-    if(i==nums.size()){
-        if(sum==0){
-            return 1;
+    int solve(int i,int sum,vector<int>&nums){
+        if(i==nums.size()){
+            //we have reached the end.
+            //check if sum==0.
+            if(sum==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
         }
-        else{
-            return 0;
+        if(dp[i][sum]!=-1){
+            return dp[i][sum];
         }
+        //bhai take krne ke lie ek codnition bhi to hai wo check kro.
+        int take=0;
+        if(nums[i]<=sum){
+         take=solve(i+1,sum-nums[i],nums);
+        }
+        int skip=solve(i+1,sum,nums);
+        return dp[i][sum]=take+skip;
     }
-    if(dp[i][sum]!=-1){
-        return dp[i][sum];
-    }
-    int take=0;
-    int skip=0;
-    //check if we can take.
-    if(nums[i]<=sum){
-        take=solve(i+1,nums,sum-nums[i]);
-    }
-    skip=solve(i+1,nums,sum);
-    return dp[i][sum]=take+skip;
-}
     int findTargetSumWays(vector<int>& nums, int target) {
+        //we need to check if there is a subset whose sum is totalsum+targetsum/2;
         int totalsum=0;
-        for(auto &x:nums){
+        for(int x:nums){
             totalsum+=x;
         }
-        memset(dp,-1,sizeof(dp));
         if(abs(target)>totalsum){
-            return false;
-        }
-        if((target+totalsum)%2!=0){
             return 0;
         }
-            int x=(target+totalsum)/2.0;
-            return solve(0,nums,x);
-        
+        if((totalsum+target)%2!=0){
+            return 0;
+        }
+        int find=(totalsum+target)/2;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,find,nums);
     }
 };
