ae08476061d6e82fbc52d3607491f77dc0338c8a
Deep Pandey <bandana.0081@gmail.com>
2026-08-02 11:06:13 +0530

    Time: 7 ms (35.23%), Space: 11.4 MB (37.89%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
 int dp[502][502];
-    int solve(int i , int j, vector<int>&nums){
+    int solve(int i,int j,vector<int>&piles){
         if(i==j){
-            //ek hee bacha hai to wahi uthana padega.
-            return nums[i];
+            return piles[i];
         }
         if(i>j){
-            return 0;//cross kr gye ek dusre ko yni array empty hai ab/
+        return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int take_i=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int take_j=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
-        //now p1 takes max from take_i and take_j. 
-        return dp[i][j]=max(take_i,take_j);
+        //take i or take j,
+        int take_i=piles[i]+min(solve(i+2,j,piles),solve(i+1,j-1,piles));
+        int take_j=piles[j]+min(solve(i+1,j-1,piles),solve(i,j-2,piles));
+        return  dp[i][j]=max(take_i,take_j);
     }
-    bool stoneGame(vector<int>& nums) {
-        int n=nums.size();
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
         memset(dp,-1,sizeof(dp));
-        int p1=solve(0,n-1,nums);
-        int totalscore=0;
-        for(int &x:nums){
-            totalscore+=x;
+        int p1=solve(0,n-1,piles);
+        int total=0;
+        for(int &x:piles){
+            total+=x;
         }
-        int p2=totalscore-p1;
-        return (p1>p2);
-        
+        int p2=total-p1;
+        return p1>p2;
     }
 };
