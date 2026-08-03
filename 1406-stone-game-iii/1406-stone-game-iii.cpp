7ba88148df269a1ba4749fcdf998329802bf5e1c
Deep Pandey <bandana.0081@gmail.com>
2026-08-03 09:14:49 +0530

    Time: 62 ms (22.92%), Space: 137 MB (59.57%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
 int dp[50001][2];
 bool vis[50001][2];
-;    int solveforalice(int i,int person,vector<int>&stoneValue){
+    int solveforalice(int i,int person, vector<int>&stoneValue){
         if(i>=stoneValue.size()){
             return 0;
         }
+        //now we have choice of picking 1,2 or 3 elements starting from ith index.
         if(vis[i][person]){
             return dp[i][person];
         }
         int stones=0;
-        int res=(person==1)?INT_MIN:INT_MAX;
+        int result=(person==1)?INT_MIN:INT_MAX;
         for(int j=1;j<=3;j++){
-            if(person==1){//a;lice ki bari hai
-            if(j+i-1<stoneValue.size()){
-            stones+=stoneValue[j+i-1];
+            if(person==1){//alice ki chance hai
+            if(i+j-1<stoneValue.size()){
+            stones+=stoneValue[i+j-1];
             }
-            res=max(res,stones+solveforalice(j+i,0,stoneValue));//apne mia best dega
+            result=max(result,stones+solveforalice(i+j,0,stoneValue));
             }
-            else{//bob ki baari hai.
-            res=min(res,solveforalice(j+i,1,stoneValue));//opponent bhi best dega to waha se worst(min) expect krega.
+            else{
+                //bob ki chance hai 
+                result=min(result,solveforalice(i+j,1,stoneValue));
             }
         }
         vis[i][person]=true;
-        return dp[i][person]=res;
+        return  dp[i][person]=result;
     }
     string stoneGameIII(vector<int>& stoneValue) {
         memset(dp,-1,sizeof(dp));
