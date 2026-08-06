c8f445d41cc34da7eba5986f642b123c5bee28f7
Deep Pandey <bandana.0081@gmail.com>
2026-08-06 14:10:14 +0530

    Time: 2 ms (4.59%), Space: 8.5 MB (5.38%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-bool valid(int n,int low,int high){
-    return (n>=low) && (n<=high);
+bool isvalid(int n,int low,int high){
+    return (n>=low && n<=high);
 }
     vector<int> sequentialDigits(int low, int high) {
         queue<int>q;
         for(int i=1;i<=8;i++){
             q.push(i);
         }
-        vector<int>res;
+        vector<int>ans;
         while(!q.empty()){
-            int n=q.front();
+            int curr=q.front();
             q.pop();
-            if(valid(n,low,high)){
-                res.push_back(n);
+            if(isvalid(curr,low,high)){
+                ans.push_back(curr);
             }
-            int ld=n%10;
-            if(ld+1<=9){
-                q.push(n*10 + (ld+1));///12--->123 mil jaygaaaa.
+            int ld=curr%10;
+            if(ld<=8){
+                q.push(curr*10+(ld+1));
             }
         }
-        return res;
+        return ans;
     }
 };
