179f8ee04b3401c9ffbea384412ea8ba0fbc48f8
Deep Pandey <bandana.0081@gmail.com>
2026-08-09 10:36:25 +0530

    Time: 22 ms (25.78%), Space: 11.8 MB (72.97%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-int dp[2][101][1001];
-    int solveforalice(int person,int i , vector<int>&piles,int M){
+int dp[100][1000][2];
+    int solveforalice(int i,int M ,vector<int>&piles,int person){
         if(i>=piles.size()){
             return 0;
         }
-        if(dp[person][i][M]!=-1){
-            return dp[person][i][M];
+        if(dp[i][M][person]!=-1){
+            return dp[i][M][person];
         }
+        //now run a loop fromm 1 to 2*M.
+        int res=(person==0)?INT_MIN:INT_MAX;
         int stones=0;
-        int res=(person==1)?-1:INT_MAX;
         for(int x=1;x<=2*M;x++){
-            if(person==1){
-                if(i+x-1<piles.size())
+            if(i+x-1<piles.size()){
             stones+=piles[i+x-1];
-            res=max(res,stones+solveforalice(0,x+i,piles,max(M,x)));
+            }
+            if(person==0){
+                //alice ki chance hai so max kro result ko
+                res=max(res,stones+solveforalice(i+x,max(M,x),piles,1));
             }
             else{
-                //bob ka chance hai.
-                res=min(res,solveforalice(1,x+i,piles,max(M,x)));
+                //if bob ki chance then wo b optimally khelega so we get mini,um.
+                res=min(res,solveforalice(i+x,max(M,x),piles,0));
             }
         }
-        return  dp[person][i][M]=res;
-
-        }
-    
+        return dp[i][M][person]=res;
+    }
     int stoneGameII(vector<int>& piles) {
         memset(dp,-1,sizeof(dp));
-        return solveforalice(1,0,piles,1);
-        
+        int score=solveforalice(0,1,piles,0);
+        return score;
     }
 };
