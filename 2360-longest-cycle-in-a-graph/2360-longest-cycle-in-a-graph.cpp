83ed32a938ae073cf99bca644fee9baf3961cb86
Deep Pandey <bandana.0081@gmail.com>
2026-08-11 21:02:02 +0530

    Time: 377 ms (8.97%), Space: 231.2 MB (5.07%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -7,56 +7,59 @@ public:
                 dfsfill(v,adj,vis,st);
             }
         }
-        st.push(node);
+        st.push(node);//pehle baccho ko dal lo fir maa ko dalna.
     }
-    void dfs_traversal(int node,vector<vector<int>>&revadj,vector<bool>&vis,int &len){
+    void makescc(int node,vector<vector<int>>&adj,vector<bool>&vis,vector<int>&currscc){
         vis[node]=true;
-        len++;
-        for(auto&v:revadj[node]){
+        currscc.push_back(node);
+        for(auto &v:adj[node]){
             if(!vis[v]){
-                dfs_traversal(v,revadj,vis,len);
+                makescc(v,adj,vis,currscc);
             }
         }
     }
     int longestCycle(vector<int>& edges) {
-        stack<int>st;
-        //first we will make adjacncy list.
+        //we havr to find the length of the largest SCC.
         int n=edges.size();
-       vector<bool>vis(n,false);
         vector<vector<int>>adj(n);
         for(int i=0;i<n;i++){
             if(edges[i]!=-1){
-            adj[i].push_back(edges[i]);
+           adj[i].push_back(edges[i]);
+            }
+
+        }
+        //now make toposort orderf.
+        stack<int>st;
+        vector<bool>vis(n,false);
+        for(int u=0;u<n;u++){
+            if(!vis[u]){
+                dfsfill(u,adj,vis,st);
             }
         }
-        //now call dfs fill for filling stakc with toporder.
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfsfill(i,adj,vis,st);
-            }
-        }
-        // Step 4: Reset visited array for the second pass
-        fill(vis.begin(), vis.end(), false);
-        //now reverse the edges.
+
+        fill(vis.begin(),vis.end(),false);
+        int mx=-1;
         vector<vector<int>>revadj(n);
         for(int u=0;u<n;u++){
             for(auto &v:adj[u]){
                 revadj[v].push_back(u);
             }
         }
-        //now do bfs in toporder.
-        int max_cycle_len=-1;
+        //now our stack has the topoorder.
+        //now do  dfs on the reversed edges graph in topoorde.r
         while(!st.empty()){
-            int node=st.top();
+            int curr=st.top();
             st.pop();
-            if(!vis[node]){
-                int curr_cycle_len=0;
-                dfs_traversal(node,revadj,vis,curr_cycle_len);
-                if(curr_cycle_len>1){//to avoid single node scc.
-                max_cycle_len=max(max_cycle_len,curr_cycle_len);
+            if(!vis[curr]){
+                vector<int>currscc;
+                makescc(curr,revadj,vis,currscc);
+                //now currscc is filled with the scc startinf from curr.
+                if(currscc.size()>1){
+                mx=max(mx,(int)currscc.size());
                 }
+
             }
         }
-        return max_cycle_len;
+        return mx;
     }
 };
