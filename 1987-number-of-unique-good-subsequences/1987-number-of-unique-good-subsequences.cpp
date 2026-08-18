469bd4f9c7b31f4010cbf19ad27cd2eb0c7eca15
Deep Pandey <bandana.0081@gmail.com>
2026-08-18 13:49:15 +0530

    Time: 7 ms (55.75%), Space: 15.1 MB (69.32%) - LeetHub

--- 1987-number-of-unique-good-subsequences/1987-number-of-unique-good-subsequences.cpp
+++ 1987-number-of-unique-good-subsequences/1987-number-of-unique-good-subsequences.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
 int MOD=1e9+7;
     int numberOfUniqueGoodSubsequences(string binary) {
-        int end0=0;
-        int end1=0;
+        int ends_w_1=0;
+        int end_w_0=0;
         bool has0=false;
         for(char &ch:binary){
             if(ch=='1'){
-                end1=(end1 + end0 +1)%MOD;
+                ends_w_1=(ends_w_1+end_w_0+1)%MOD;
             }
             else{
                 has0=true;
-                end0=(end1+end0)%MOD;
+                end_w_0=(end_w_0+ends_w_1)%MOD;
             }
         }
-        return (has0)?(1+ end0+end1)%MOD:(end0+end1)%MOD;
+        return (has0)?(ends_w_1+end_w_0+1)%MOD:(ends_w_1+end_w_0)%MOD;
     }
 };
