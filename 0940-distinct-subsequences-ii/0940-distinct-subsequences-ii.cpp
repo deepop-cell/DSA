08076a13f1aa8bfd2e48e98b688bd7b9986f8d0e
Deep Pandey <bandana.0081@gmail.com>
2026-08-18 14:35:29 +0530

    Time: 3 ms (35.84%), Space: 8.9 MB (81.77%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
 int MOD=1e9+7;
     int distinctSubseqII(string s) {
-        //nromal rec TLE dega kyuki 2 pwoer n legenege aur hum temp ki wjh se use memoise b nhi kr skte hai, to kya kre ab>?
-        //1987 wala way, jab bhi koi char aa rha h to tumm  use sare 26 chars se end hone wale subs mai add kr kste +1 additional , ( wo char akela)..
-        vector<int>end_w(26,0);
+        vector<int>end_w(26,0);//this means end_w[i]--> no of subsqe ending with a+'i char.
         for(char &ch:s){
             long long total=0;
             for(int &x:end_w){
-                total=(total+x)%MOD;
+                total+=x;
             }
-            end_w[ch-'a']=(total+1)%MOD;//total mai end_w[ch-'a] already hai to usme alag se add krne ki jrruat nai  hai.
+            end_w[ch-'a']=(total+1)%MOD;
         }
         long long ans=0;
         for(int &x:end_w){
-            ans+=x;
+                ans+=x;
         }
-        return ans%MOD;
+    return ans%MOD;
     }
 };
