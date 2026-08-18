36e006e669b2ab6171cec4689cc413d0de12e51d
Deep Pandey <bandana.0081@gmail.com>
2026-08-18 10:49:55 +0530

    Time: 49 ms (82.77%), Space: 13.3 MB (82.43%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-      //idea ye hai ki . lps is lcs of string and its reverse.
-      string x=s;
-      reverse(x.begin(),x.end());
-      //state def : dp[i][j]=LCS len of s1 of len i and s2 of len j/...
-      int n=s.length();
-      vector<vector<int>>dp(n+1,vector<int>(n+1));
-      for(int i=0;i<=n;i++){
-        for(int j=0;j<=n;j++){
-            //agar ek b string empty hua to lcs 0 ho jayga.
-            if(i==0 || j==0 ){
-                dp[i][j]=0;
-            }
-            else if(s[i-1]==x[j-1]){
-                dp[i][j]=1+dp[i-1][j-1];
-            }
-            else{
-                //unequl hai..
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+int dp[1005][1005];
+    int solve(int i,int j,string&s1,string&s2,int n,int m){
+        if(i>=n || j>=m){
+            return 0;
         }
-      }
-      return dp[n][n];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        //now if current char mathes then move ahead in both
+        if(s1[i]==s2[j]){
+          return dp[i][j]= 1+solve(i+1,j+1,s1,s2,n,m);
+        }
+        else{
+          return dp[i][j]= max(solve(i+1,j,s1,s2,n,m),solve(i,j+1,s1,s2,n,m));
+        }
+    }
+    int longestPalindromeSubseq(string s) {
+        int n=s.length();
+        //its just lcs of s and revs
+        string revs=s;
+        reverse(revs.begin(),revs.end());
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,s,revs,n,n);
     }
 };
