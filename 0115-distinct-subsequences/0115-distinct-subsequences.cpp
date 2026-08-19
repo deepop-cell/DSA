4d5e40d01a816fcec85a75160acc964e4e5fcdd1
Deep Pandey <bandana.0081@gmail.com>
2026-08-19 10:11:11 +0530

    Time: 31 ms (53.18%), Space: 13.2 MB (78.33%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-int dp[1001][1001];
-    int solve(int i, int j,string&s,string&t){
-        // i for s and j for t
+int dp[1002][1002];
+    int solve(int i,int j ,string&s,string&t){
         if(j==t.length()){
-            //we have formed one.
-            return 1;
+            return 1;//one way found
         }
-        if(i>=s.length()){
+        if(i==s.length()){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        //now we are at current index i and we have option to include it or not include it.
         int take=0;
         if(s[i]==t[j]){
-           take=solve(i+1,j+1,s,t);
+            take=solve(i+1,j+1,s,t);
         }
         int skip=solve(i+1,j,s,t);
-        return dp[i][j]=take+skip;
-        
+        return dp[i][j]=skip+take;
     }
     int numDistinct(string s, string t) {
         memset(dp,-1,sizeof(dp));
