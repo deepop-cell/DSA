d1306abe62592357a794c69bfc52978ffb17e33e
Deep Pandey <bandana.0081@gmail.com>
2026-08-19 19:19:57 +0530

    Time: 3 ms (68.69%), Space: 13 MB (83.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-
-    void solve(int open_count,int close_count,vector<string>&temp,string &res,int n){
-        if(open_count==n && close_count==n){
-            temp.push_back(res);
+    void solve(int openc,int closec,vector<string>&res,string&temp,int n){
+        if(openc==n && closec==n){
+            res.push_back(temp);
             return;
         }
-        //now we have choice to open or close a bracket.
-        if(close_count<open_count){
-            res+=')';
-            solve(open_count,close_count+1,temp,res,n);
-            res.pop_back();
+        //now u have option  to close or open bracket.
+        if(openc<n){
+            temp+='(';
+            solve(openc+1,closec,res,temp,n);
+            temp.pop_back();//backtracking
         }
-        if(open_count<n){
-            res+='(';
-            solve(open_count+1,close_count,temp,res,n);
-            res.pop_back();
+        if(openc>closec){
+            temp+=')';
+            solve(openc,closec+1,res,temp,n);
+            temp.pop_back();
         }
-        
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>temp;
-        string res="";
-        solve(0,0,temp,res,n);
-        return temp;
+        string temp="";
+        vector<string>res;
+        solve(0,0,res,temp,n);
+        return res;
     }
 };
