ef738e7495f05debe20428c5f7266860c3a9a824
Deep Pandey <bandana.0081@gmail.com>
2026-08-21 14:36:11 +0530

    Time: 0 ms (100%), Space: 21.6 MB (35.82%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool solve(int targetsum, TreeNode* root) {
         if (root == NULL) return false;
-        targetsum -= root->val;
         if (root->left == NULL && root->right == NULL) {
             if(targetsum==0){
                 return true;
@@ -14,14 +13,17 @@ public:
         bool go_left = false;
         bool go_right = false;
         if (root->left != NULL) {
-            go_left = solve(targetsum, root->left);
+            go_left = solve(targetsum-root->left->val, root->left);
         }
         if (root->right != NULL) {
-            go_right = solve(targetsum, root->right);
+            go_right = solve(targetsum-root->right->val, root->right);
         }
         return (go_left || go_right);
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return solve(targetSum, root);
+        if(!root){
+            return false;
+        }
+        return solve(targetSum-root->val, root);
     }
 };
