e349dd99be0b63699f755079df1a0cbd8918dbd0
Deep Pandey <bandana.0081@gmail.com>
2026-08-24 11:15:23 +0530

    Time: 0 ms (100%), Space: 21.4 MB (96.08%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,29 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool solve(int targetsum, TreeNode* root) {
-        if (root == NULL) return false;
-        if (root->left == NULL && root->right == NULL) {
-            if(targetsum==0){
+    bool solve(TreeNode* root,int sum){
+        if(!root){
+            return false;
+        }
+        if(!root->left && !root->right){
+            if(sum==0){
                 return true;
             }
-            else{
-                return false;
-            }
+            return false;
         }
-        bool go_left = false;
-        bool go_right = false;
-        if (root->left != NULL) {
-            go_left = solve(targetsum-root->left->val, root->left);
+        //now we have options to go left or go right
+        bool l=false;
+        bool r=false;
+        if(root->left){
+           l= solve(root->left,sum-root->left->val);
         }
-        if (root->right != NULL) {
-            go_right = solve(targetsum-root->right->val, root->right);
+        if(root->right){
+            r=solve(root->right,sum-root->right->val);
         }
-        return (go_left || go_right);
+        return r || l;
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root){
             return false;
         }
-        return solve(targetSum-root->val, root);
+        return solve(root,targetSum-root->val);
     }
 };
