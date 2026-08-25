0c7985540d1100083e5191635126431d7ad38f32
Deep Pandey <bandana.0081@gmail.com>
2026-08-25 11:06:22 +0530

    Time: 0 ms (100%), Space: 10.6 MB (51.6%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void solve(int idx,vector<int>&nums,vector<vector<int>>&res){
-        if(idx==nums.size()){
-            //means u have sucessfully formed one permuation of nums.
+    void solve(int i,vector<vector<int>>&res,vector<int>&nums){
+        if(i==nums.size()){
+            //we have formed one succesdsful permutation.
             res.push_back(nums);
             return;
         }
-        //permuations is just swappping wali chiz so try swapping all pairs.
-        for(int i=idx;i<nums.size();i++){//we start swapping from current pos because an element can stay at its own pos too
-            swap(nums[i],nums[idx]);
-            //recursive call for ahead for swappping.
-            solve(idx+1,nums,res);
-            swap(nums[idx],nums[i]);//revert it(backtracking wali chieez).
+        ///perm is just swap of postiotn so try every 
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            solve(i+1,res,nums);
+            swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>res;
-        solve(0,nums,res);
+        solve(0,res,nums);
         return res;
-        
     }
 };
