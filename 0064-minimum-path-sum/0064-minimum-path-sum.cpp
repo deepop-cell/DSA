5f5d1946aeb27d216a9c7861b22f0b5091bd5ed3
Deep Pandey <bandana.0081@gmail.com>
2026-08-26 21:49:46 +0530

    Time: 0 ms (100%), Space: 15.6 MB (81.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+int dp[201][201];
+    int solve(int i,int j,vector<vector<int>>&grid){
+        if(i>=grid.size() || j>=grid[0].size()){
+            return 1e9;
+        }
+        if(i==grid.size()-1 && j==grid[0].size()-1){
+            return grid[i][j];
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        //now here we have two opiotns ya to down ya to right
+        int right=solve(i,j+1,grid);
+        int down=solve(i+1,j,grid);
+        return dp[i][j]=grid[i][j]+min(right,down);
+    }
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid[0].size();
-        int m=grid.size();
-        vector<vector<int>>dp(m,vector<int>(n));
-        //state defn:dp[i][j]=min path sum from [0,0] to [i,j]...
-        dp[0][0]=grid[0][0];
-        for(int j=1;j<n;j++){
-            dp[0][j]=(dp[0][j-1]+grid[0][j]);
-        }
-        for(int i=1;i<m;i++){
-            dp[i][0]=(dp[i-1][0]+grid[i][0]);
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
-            }
-        }
-        return dp[m-1][n-1];
-        
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,grid);
     }
 };
