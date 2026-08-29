2b12b37d585dd56542d58581c25337f7fc46970e
Deep Pandey <bandana.0081@gmail.com>
2026-08-29 13:10:19 +0530

    Time: 121 ms (92.96%), Space: 140.7 MB (88.45%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         int n=nums.size();
-        vector<pair<int,int>> temp(n);
+        vector<pair<int,int>>temp(n);
         for(int i=0;i<n;i++){
             temp[i]={nums[i],i};
         }
@@ -10,22 +10,19 @@ public:
         int i=0;
         while(i<n){
             int j=i+1;
-            // Expand group while adjacent difference <= limit
-            while(j<n&&temp[j].first-temp[j-1].first<=limit){
-                j++;
+            while(j<n && temp[j].first-temp[j-1].first<=limit){
+                j++;//expand the group/
             }
-            // Collect and sort original index positions
-            vector<int> indices;
+            vector<int>indices;
             for(int k=i;k<j;k++){
                 indices.push_back(temp[k].second);
             }
             sort(indices.begin(),indices.end());
-            // Place sorted values into sorted original index slots
             for(int k=0;k<indices.size();k++){
                 nums[indices[k]]=temp[i+k].first;
             }
-            i=j; // Move to next component
+            i=j;//next grp par jaao.
         }
-        return nums;
+     return nums;   
     }
 };
