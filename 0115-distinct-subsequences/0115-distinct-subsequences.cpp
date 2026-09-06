cbe8c47def3ee651166002c1602859f052f2270d
Deep Pandey <bandana.0081@gmail.com>
2026-09-06 12:01:49 +0530

    Time: 30 ms (58.87%), Space: 13 MB (79.71%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
 int dp[1002][1002];
-    int solve(int i,int j ,string&s,string&t){
-        if(j==t.length()){
-            return 1;//one way found
-        }
-        if(i==s.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int take=0;
-        if(s[i]==t[j]){
-            take=solve(i+1,j+1,s,t);
-        }
-        int skip=solve(i+1,j,s,t);
-        return dp[i][j]=skip+take;
+int solve(int i ,int j , string &s , string &t){
+    if(j==t.length()){
+        return 1;//ek raasta mil gaya.
     }
+    if(i==s.length()){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int take=0;
+    if(s[i]==t[j]){
+        take=solve(i+1,j+1,s,t);
+    }
+    int skip=solve(i+1,j,s,t);
+    return dp[i][j]=take+skip;
+}
     int numDistinct(string s, string t) {
         memset(dp,-1,sizeof(dp));
         return solve(0,0,s,t);
-        
     }
 };
