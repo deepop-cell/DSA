e95a806a853d15f6ab33bec228ab5f34cb4b086d
Deep Pandey <bandana.0081@gmail.com>
2026-09-07 15:46:13 +0530

    Time: 5 ms (18.08%), Space: 8.8 MB (78.81%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 int MOD=1e9+7;
     int distinctSubseqII(string s) {
-        vector<int>end_w(26,0);//this means end_w[i]--> no of subsqe ending with a+'i char.
+        vector<int>end_w(26,0);
         for(char &ch:s){
             long long total=0;
             for(int &x:end_w){
@@ -12,8 +12,9 @@ int MOD=1e9+7;
         }
         long long ans=0;
         for(int &x:end_w){
-                ans+=x;
+            ans+=x;
         }
-    return ans%MOD;
+        return ans%MOD;
+        
     }
 };
