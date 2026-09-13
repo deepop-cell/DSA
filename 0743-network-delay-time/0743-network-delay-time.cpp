b4f36b6ab81bfff52b8d7aa17f5556ef54fe5e3b
Deep Pandey <bandana.0081@gmail.com>
2026-09-13 18:24:51 +0530

    Time: 78 ms (74.15%), Space: 44.4 MB (70.51%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n+1);
-    //first make an adjacnecy list
-    for(auto &e:times){
-        int u=e[0];
-        int v=e[1];
-        int wt=e[2];
-        adj[u].push_back({v,wt});
-    }
-    vector<int>result(n+1,INT_MAX);
-    result[k]=0;
-    //make a minheap.
-   priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0,k});//dist , node.
-    while(!pq.empty()){
-        int node=pq.top().second;
-        int d=pq.top().first;
-        pq.pop();
-        for(auto &temp:adj[node]){
-            int adjnode=temp.first;
-            int wt=temp.second;
-            if(d+wt<result[adjnode]){
-                result[adjnode]=d+wt;
-                pq.push({d+wt,adjnode});
+    vector<vector<pair<int, int>>> adj(n+1);
+        for(auto &x:times){
+            int a=x[0];
+            int b=x[1];
+            int wt=x[2];
+            adj[a].push_back({b,wt});
+        }
+        vector<int>dist(n+1,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        dist[k]=0;
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int node=it.second;
+            int d=it.first;
+            if(d>dist[node]){
+                continue;
+            }
+            for(auto &temp:adj[node]){
+                int adjnode=temp.first;
+                int wt=temp.second;
+                if(d+wt<dist[adjnode]){
+                    dist[adjnode]=d+wt;
+                    pq.push({d+wt,adjnode});
+                }
             }
         }
-
-    }
-    int max_time=0;
-    for(int i=1;i<result.size();i++){
-        if(result[i]==INT_MAX){
-            //means node was never reached
-            return -1;
+        int mx=INT_MIN;
+        for(int i=1;i<=n;i++){
+            if(dist[i]==INT_MAX){
+                return -1;
+            }
+            else{
+                mx=max(mx,dist[i]);
+            }
         }
-        else{
-            max_time=max(max_time,result[i]);
-        }
-    }
-    return max_time;
+        return mx;
     }
 };
