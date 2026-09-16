84f879a84c724e79e9aa90e1c4e1e64a4eec22af
Deep Pandey <bandana.0081@gmail.com>
2026-09-16 11:26:05 +0530

    Time: 134 ms (22.9%), Space: 30.2 MB (45.33%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-int dp[1002][1002][4];
 int N;
+int dp[1005][1005][5];
 int mod=1e9+7;
 int solve(int i,int k,int covered){
     if(k==0){
-        return covered==0;//means no open segments are left now
+        if(covered==0){
+            return 1;
+        }
+        return 0;
     }
     if(i==N){
         return 0;
@@ -13,25 +16,26 @@ int solve(int i,int k,int covered){
     if(dp[i][k][covered]!=-1){
         return dp[i][k][covered];
     }
-    //now we have option, either to extend this segment or start a new segment.
-    int skip=0;
     int start=0;
+    int dontstart=0;
     if(covered==0){
-        //we have 2 choices, ya to new segment start kro ya to isse bhi skip kardo.
-        skip=solve(i+1,k,0);
-        start=solve(i+1,k,1);
-        return dp[i][k][covered]= (skip+start)%mod;
+    //we have two options , start a segment from current or skip/
+    start=solve(i+1,k,1);
+    dontstart=solve(i+1,k,0);
+    return dp[i][k][covered]=(start+dontstart)%mod;
     }
-    int extend=solve(i+1,k,min(covered+1,2));
+    int extend=0;
     int end=0;
-    if(covered==2 || covered==1){
-    end=solve(i,k-1,0);//we can also start a new seg from here or not so keep i hereonly
+     if(covered==1 || covered==2){
+        //we can either extend this segment now or end this segment here/
+     extend=solve(i+1,k,min(2,covered+1));
+     end=solve(i,k-1,0);//in end we dont do i+1 because shared endpoints is allowed in the qsn.
     }
     return dp[i][k][covered]=(extend+end)%mod;
 }
     int numberOfSets(int n, int k) {
         memset(dp,-1,sizeof(dp));
         N=n;
-        return solve(0,k,0);
+       return solve(0,k,0);
     }
 };
