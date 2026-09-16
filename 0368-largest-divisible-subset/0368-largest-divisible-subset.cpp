83d8ced807784417ad69e626eba75ebfd244445c
Deep Pandey <bandana.0081@gmail.com>
2026-09-16 23:13:57 +0530

    Time: 12 ms (42.08%), Space: 12.6 MB (51.65%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n=nums.size();
+        if(nums.size()==1){
+            return nums;
+        }
         sort(nums.begin(),nums.end());
-        vector<int>previdx(n,-1);
-        int maxL=1;
-        int maxl_idx=0;
+        int n=nums.size();
+        int end=-1;
+        int mx=0;
         vector<int>dp(n,1);
+        vector<int>parent(n,-1);
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[j]%nums[i]==0 || nums[i]%nums[j]==0){
-                    if(dp[j]+1>dp[i]){
-                        dp[i]=dp[j]+1;
-                        previdx[i]=j;
+                if(nums[i]%nums[j]==0){
+                  //we can inlcude this one.
+                  if(dp[j]+1>dp[i]){
+                    dp[i]=dp[j]+1;
+                    parent[i]=j;
+                    if(dp[i]>mx){
+                        mx=dp[i];
+                        end=i;
                     }
-                    if(dp[i]>maxL){
-                        maxL=dp[i];
-                        maxl_idx=i;
-                    }
-
-
+                  }
                 }
-
             }
         }
-        //now i have maxL index and previous index. 
-        vector<int>result;
-        int i=maxl_idx;
-        while(previdx[i]!=-1){
-            result.push_back(nums[i]);
-            i=previdx[i];
+        if(end==-1){
+            return {nums[0]};
         }
-        //jab previdx[i]=-1 hai usko to push hee nhi kiya lol.
-        result.push_back(nums[i]);
-        reverse(result.begin(),result.end());
-        return result;        
+        vector<int>ans;
+        int k=end;
+        while(parent[k]!=-1){
+         ans.push_back(nums[k]);
+         k=parent[k];
+        }
+        ans.push_back(nums[k]);
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
