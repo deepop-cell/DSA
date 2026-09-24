9747fb85e4439807c482894b6fc0686e376613d3
Deep Pandey <bandana.0081@gmail.com>
2026-09-24 14:06:23 +0530

    Time: 23 ms (28.1%), Space: 25.7 MB (33.8%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+vector<vector<int>>directions={{1,0},{-1,0},{0,1},{0,-1}};
+    int maxDistance(vector<vector<int>>& grid) {
+        //start multi source bfs from all land cells .
+        int n=grid.size();
+        int lc=0;
+        int wc=0;
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+            if(grid[i][j]==1){
+                //its land 
+                q.push({i,j});
+                lc++;
+            }
+            else{
+                wc++;
+            }
+            }
+        }
+        if(lc==n*n || wc==n*n){
+            return -1;
+        }
+        //now start bfs 
+        int dist=0;
+        vector<vector<bool>>vis(n,vector<bool>(n,false));
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                auto it=q.front();
+                int r=it.first;
+                int c=it.second;
+                q.pop();
+                for(auto &dir:directions){
+                    int new_r=r+dir[0];
+                    int new_c=c+dir[1];
+                    if(new_r<0 || new_r>=n || new_c<0 || new_c>=n){
+                        ///out of bounds bahar aa gye matrix ke
+                        continue;
+                    }
+                    if(!vis[new_r][new_c]){
+                        q.push({new_r,new_c});
+                        vis[new_r][new_c]=true;
+                    }
+                }
+
+            }
+            if(!q.empty()){
+                dist++;
+            }
+        }
+        return dist;
+    }
+};
